4f1d07dab096f9a6aad9cd20df348aeafa7d2a58
emreatasoy <yunusatasoyy16@gmail.com>
2025-07-29 23:18:05 +0300

    Login ve Succes component temeli oluşturuldu.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,42 @@
+import { Button, Form, FormGroup, FormText, Input, Label } from "reactstrap";
+
 export default function Login() {
     return (
 
-        <><p>Loginn</p></>
+        <Form>
+  <FormGroup>
+    <Label for="email">
+      Email:
+    </Label>
+    <Input
+      id="email"
+      name="email"
+      placeholder="Emailinizi giriniz."
+      type="email"
+    />
+  </FormGroup>
+  <FormGroup>
+    <Label for="password">
+      Password:
+    </Label>
+    <Input
+      id="password"
+      name="password"
+      placeholder="Parolanızı giriniz."
+      type="password"
+    />
+  </FormGroup>
+ 
+  <FormGroup check>
+    <Input type="checkbox" />
+    {' '}
+    <Label check>
+      Kabul Ediyorum
+    </Label>
+  </FormGroup>
+  <Button>
+    Giriş Yap
+  </Button>
+</Form>
     )
 }
---
+++ src/components/Success.jsx
@@ -0,0 +1,8 @@
+export default function Succes() {
+    return (
+        <>
+      <h2>Giriş Başarılı!</h2>
+      <p>Başarıyla giriş yaptınız. Hoş geldiniz! 🎉</p>
+    </>
+    )
+}
